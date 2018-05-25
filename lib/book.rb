3b2ab3e3ab7b234ec833b35cf1f34b81a4bb633a
George  Pianka <gpianka1@gmail.com>
2018-05-25 05:07:53 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,7 +7,21 @@ class Book
     @title
   end
   
+  def author=(author)
+    @author = author
+  end
+  
   def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
+  end
 
 
 
